62087e0ebd6c031b7887ae1addc345ed45d9b214
Ashif Asker <ashif6@gmail.com>
2022-10-08 17:35:01 +0530

    Add project files.

---
+++ eSports/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eSports.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
